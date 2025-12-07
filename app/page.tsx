c2c4559affa7ebb418abbd115f39d845456d4ec8
mhamdany <mhamdanydev@gmail.com>
2025-12-07 23:04:01 +0700

    add food item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import Image from "next/image";
 import food_1 from "@/public/img/food-1.png";
 import food_2 from "@/public/img/food-2.png";
 import food_3 from "@/public/img/food-3.png";
+import category_1 from "@/public/img/category_1.png";
+import category_2 from "@/public/img/category_2.png";
+import category_3 from "@/public/img/category_3.png";
+import category_4 from "@/public/img/category_4.png";
+import { CheckCircle, CheckLine } from "lucide-react";
 
 export default function Home() {
   return (
@@ -25,35 +30,35 @@ export default function Home() {
           <h1 className="title text-xs md:text-[20px] lg:text-3xl font-bold max-w-xl mt-10 md:mt-0 tracking-widest leading-10">
             Its Quick & Amusing!
           </h1>
-          <p className="text-white text-2xl md:text-4xl lg:text-6xl font-bold mt-1 max-w-md">
-            <span className="text-chart-5">Th</span>e art of speed
+          <p className="text-white text-2xl md:text-4xl lg:text-6xl font-bold mt-1">
+            <span className="text-chart-5">Th</span>e Art of Speed
             <br />
-            food quality
+            Food Quality
           </p>
-          <p className="text-[7px] md:text-[10px] lg:text-[16px] text-white font-thin mt-4 leading-2 lg:leading-6">
+          <p className="text-[7px] md:text-[10px] lg:text-[16px] text-white font-thin mt-4 leading-2.5 md:leading-4 lg:leading-6">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
             <br />
             Libero, ab quos. Quae eveniet rerum perspiciatis mollitia dolorem
             eius
           </p>
 
-          <button className="mt-6 bg-chart-5 text-white px-4 py-2 rounded-full text-[7px] md:text-[10px] lg:text-[16px] w-fit hover:bg-neutral-200">
-            Lihat Menu
+          <button className="mt-6 bg-chart-5 text-white px-4 py-2 rounded-full text-[7px] md:text-[10px] lg:text-[16px] w-fit hover:bg-chart-4">
+            See Menu
           </button>
         </div>
       </section>
       <section className="content px-4 py-5 lg:py-10">
         <div className="grid gap-2.5 lg:grid-cols-2">
-          <div className="fle">
+          <div className="">
             <h1 className="title text-xs md:text-[20px] lg:text-3xl font-bold max-w-xl mt-10 md:mt-0 tracking-widest leading-10">
               About Us
             </h1>
-            <p className="text-white text-2xl md:text-4xl lg:text-6xl font-bold mt-1 max-w-md">
-              <span className="text-chart-5">We</span> created the best
+            <p className="text-white text-2xl md:text-4xl lg:text-6xl font-bold mt-1">
+              <span className="text-chart-5">We</span> Created the Best
               <br />
-              foody product
+              Foody Product
             </p>
-            <p className="text-[7px] md:text-[10px] lg:text-[16px] text-white font-thin mt-4 leading-2 lg:leading-6">
+            <p className="text-[7px] md:text-[10px] lg:text-[16px] text-white font-thin mt-4 leading-2.5 md:leading-4 lg:leading-6">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
               molestiae, distinctio perferendis, rerum voluptates eos, placeat
               enim neque corporis eligendi velit! Tempore suscipit, explicabo
@@ -61,22 +66,45 @@ export default function Home() {
               quaerat praesentium pariatur laudantium, culpa, consequatur
               laborum blanditiis dolorum necessitatibus. Temporibus eveniet odio
               quasi veritatis iure soluta mollitia quam, corporis nobis omnis
-              amet ut, fuga id corrupti quod nam accusantium. Numquam nostrum
-              soluta quaerat quo ipsum, maiores dolor corrupti sint accusamus,
-              neque nobis odio culpa ex natus impedit! Doloremque quos,
-              provident tenetur veritatis deserunt ullam, dolorum, maxime nam
-              vero quasi iste nemo quod. Ratione, beatae.
+              amet ut, fuga id corrupti quod nam accusantium.
             </p>
+            <ul className="mt-5 text-white text-[7px] md:text-[10px] lg:text-[16px] flex flex-col gap-2">
+              <li className="flex gap-2 items-center">
+                <CheckCircle className="size-4" /> Lorem ipsum dolor sit amet
+                consectetur adipisicing.
+              </li>
+              <li className="flex gap-2  items-center">
+                <CheckCircle className="size-4" /> Lorem ipsum dolor sit amet
+                consectetur adipisicing.
+              </li>
+              <li className="flex gap-2 items-center">
+                <CheckCircle className="size-4" /> Lorem ipsum dolor sit amet
+                consectetur adipisicing.
+              </li>
+            </ul>
+            <button className="mt-6 bg-chart-5 text-white px-4 py-2 rounded-full text-[7px] md:text-[10px] lg:text-[16px] w-fit hover:bg-chart-4">
+              Read More
+            </button>
           </div>
           <div className="grid grid-cols-2 gap-2.5 text-white mt-4 md:mt-0">
-            <span className="col-span-2">
-              <Image src={food_1} alt="Background Hero" priority />
-            </span>
-            <span className="">
-              <Image src={food_2} alt="Background Hero" priority className="" />
-            </span>
-            <div className="">
-              <Image src={food_3} alt="Background Hero" priority className="" />
+            <Image src={food_1} alt="Background Hero" className="col-span-2" />
+            <Image src={food_2} alt="Background Hero" className="" />
+            <Image src={food_3} alt="Background Hero" className="" />
+          </div>
+        </div>
+        <div>
+          <div className="text-center">
+            <h1 className="title text-xs md:text-[20px] lg:text-3xl font-bold max-w-xl mt-10 md:mt-0 tracking-widest leading-10">
+              Food Category
+            </h1>
+            <p className="text-white text-2xl md:text-4xl lg:text-6xl font-bold mt-1">
+              <span className="text-chart-5">Ch</span>oose Food Item
+            </p>
+            <div className="grid justify-center mt-5 gap-2.5">
+              <Image src={category_1} alt="category" className="" />
+              <Image src={category_2} alt="category" className="" />
+              <Image src={category_3} alt="category" className="" />
+              <Image src={category_4} alt="category" className="" />
             </div>
           </div>
         </div>
